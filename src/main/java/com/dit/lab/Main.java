641e0c3b4cd44a03ace76ab81f74f039186575b8
Linimol <linianilkumar@gmail.com>
2023-09-05 16:41:31 +0530

    Revert "Revert "add all files""
    This reverts commit ae06a21e10469d8fd7cf10268f13ce21e53801c1.

---
+++ src/main/java/com/dit/lab/Main.java
@@ -0,0 +1,10 @@
+package com.dit.lab;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to java world");
+	}
+
+}
